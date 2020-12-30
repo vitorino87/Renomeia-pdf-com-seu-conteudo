3f0ba447e7437601c5a3e2c76557280e51455dac
Tiago Vitorino <brother_87_22@hotmail.com>
2020-12-29 21:49:01 -0300

    primeiro commit

---
+++ Converte_PDF_Texto/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Converte_PDF_Texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.btnProcurar = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnProcurar
+			// 
+			this.btnProcurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnProcurar.Location = new System.Drawing.Point(164, 13);
+			this.btnProcurar.Name = "btnProcurar";
+			this.btnProcurar.Size = new System.Drawing.Size(107, 23);
+			this.btnProcurar.TabIndex = 1;
+			this.btnProcurar.Text = "Procurar...";
+			this.btnProcurar.UseVisualStyleBackColor = true;
+			this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(123, 16);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Escolha o diretório:";
+			// 
+			// ofd1
+			// 
+			this.ofd1.FileName = "openFileDialog1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 57);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(154, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Obs: Os pdfs devem ser PDF-A";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.ClientSize = new System.Drawing.Size(283, 79);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnProcurar);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Mova, extraia e renomeia";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog ofd1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Converte_PDF_Texto/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Converte_PDF_Texto
+{
+    public partial class Form1 : Form
+    {
+		public List<String> fqn = new List<string>();
+		List<String> SubDirs = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertePDF_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+			fqn.Clear();			
+			DialogResult dr2 = folderBrowserDialog1.ShowDialog();
+			fqn.Add(folderBrowserDialog1.SelectedPath);						
+			inFolder(fqn);            
+        }
+
+		private List<string> GetOthersFolder(string folder)
+		{
+			try
+			{
+				List<string> dir = Directory.GetDirectories(folder).ToList<string>();
+				return dir;
+			}catch (Exception e)
+			{ return null; }			
+		}
+
+		private void inFolder(List<string> list)
+		{
+			foreach(String s in list)
+			{
+				List<string> a = GetOthersFolder(s);
+				if (a != null)
+				{
+					inFolder(a); //pilha				
+					moveFile(s);
+				}
+			}
+		}
+
+		private void moveFile(String s)
+		{
+			List<string> b = Directory.GetFiles(s).ToList<string>();
+			int cont = 1;
+			Boolean t = false;
+			foreach (String c in b)
+			{
+				if (c.Contains(".pdf"))
+				{
+					//char[] k = "\\".ToCharArray();
+					//string[] a = c.Split(k);
+					//string h = a.Last<string>();
+
+					try
+					{																		
+						Directory.Move(c, fqn[0] + "\\" + GetNewName(c) + ".pdf");						
+					}
+					catch (System.IO.IOException e)
+					{
+						try
+						{
+							Directory.Move(c, fqn[0] + "\\" + GetNewName(c) + "(" + cont++ + ")" + ".pdf");
+						}
+						catch(System.IO.IOException f)
+						{
+							t = true;
+						}
+						
+					}
+				}					 																	
+			}
+			if (t)
+			{
+				MessageBox.Show("Não foi possível alterar alguns titulos (já estavam alterados)");
+			}
+		}
+
+		private String GetNewName(String s)
+		{
+			ConvertePDF pdftxt = new ConvertePDF();
+			String titulo = pdftxt.ExtrairTexto_PDF(s).Substring(0,30);
+			titulo = titulo.Replace("\n","%");
+			return titulo;
+		}
+
+		
+	}
+}
